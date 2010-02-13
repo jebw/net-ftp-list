537af3dc60152cccd13d47c653d01f9bcc1b9fc4
jebw <jeb@jdwilkins.co.uk>
2010-02-13 08:36:41 +0000

    Make Net::FTP.list return List parsed objects instead of strings

--- lib/net/ftp/list.rb
+++ lib/net/ftp/list.rb
@@ -33,11 +33,24 @@ module Net #:nodoc:
     #
     # None at this time. At worst you'll end up with an Net::FTP::List::Unknown instance which won't have any extra
     # useful information. Methods like <tt>dir?</tt>, <tt>file?</tt> and <tt>symlink?</tt> will all return +false+.
+    
     module List
       def self.parse(*args)
         Parser.parse(*args)
       end
     end
+    
+    # Extend original list method to return Net::FTP::List objects instead of Strings
+    
+    alias_method :orig_list, :list
+    def list(path)
+      orig_list(path) do |e|
+        yield Net::FTP::List.parse(e)
+      end
+    end
+    
+    alias_method :ls, :list
+    alias_method :dir, :list    
   end
 end
 
